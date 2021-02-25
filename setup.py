282d9d95b7f27ae661d6bfa4211e40f153ce2ac2
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2021-02-25 10:56:29 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='flexidrift',
+    version='0.1.0',
+    packages=[''],
+    url='',
+    license='GPL version 3',
+    author='Pål Næverlid Sævik',
+    author_email='pnsaevik@gmail.com',
+    description='Flexible framework for drift simulations in the ocean'
+)
